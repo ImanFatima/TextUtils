47ca192c798365be2ecff03160f989279c9612f0
ImanFatima <48816734+ImanFatima@users.noreply.github.com>
2024-12-30 17:05:20 +0500

    Add files via upload
    updated files

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 // let name ="÷"
 function App() {
   
   const [ mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null)
+  const showAlert=(message, type)=>{
+    setAlert({
+      msg: message,
+      kind: type
+    })
+    setTimeout(() => {
+        setAlert(null)
+    }, 2000);
+  }
   const toggleMode = ()=>{
     if (mode === 'light')
     { 
       setMode('dark')
-      document.body.style.backgroundColor = '#040438'
+      document.body.style.backgroundColor = '#040438';
+      showAlert("Dark mode has been enabled!!!", "success");
     }
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white'
+      showAlert("Light mode has been enabled!!!", "success");
+
     }
 
   }
@@ -24,6 +38,7 @@ function App() {
      <Navbar title="TextUtils" aboutUs="Abouts" home="Home" search="Search" mode={mode} toggleMode={toggleMode} />
      {/* <Navbar title="TextUtils"  home="Home"/>
      <Navbar/> */}
+     <Alert alert={alert}/>
      <div className="container">
         <TextForm mode={mode}/>
      </div>
