b6706741b56f2671f8e787c68bb006569e399c4d
ImanFatima <48816734+ImanFatima@users.noreply.github.com>
2024-12-31 13:01:31 +0500

    Add files via upload
    Update the existing components accordingly & add the about component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
+
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
@@ -14,13 +15,22 @@ export default function Navbar(props) {
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="/">{props.home}</a>
           </li>
-          {/* <li className="nav-item">
+          <li className="nav-item">
             <a className="nav-link" href="/">{props.aboutUs}</a>
-          </li> */}
+          </li>
         </ul>
         <div className={`form-check form-switch text-${props.mode ==='light' ? 'dark' : 'light'}`}>
         <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
             <label className="form-check-label" htmlFor="flexSwitchCheckDefault">DarkMode</label>
+        </div>
+
+        <div className="d-flex ms-3">
+            <button className="color-btn me-2" onClick={() => props.changeBgColor('#048484')} style={{ backgroundColor: '#048484' }}></button>
+            <button className="color-btn me-2" onClick={() => props.changeBgColor('#89b1c4')} style={{ backgroundColor: '#89b1c4' }}></button>
+            <button className="color-btn me-2" onClick={() => props.changeBgColor('#7c8e75')} style={{ backgroundColor: '#7c8e75' }}></button>
+            <button className="color-btn me-2" onClick={() => props.changeBgColor('#022348')} style={{ backgroundColor: '#022348' }}></button>
+            <button className="color-btn me-2" onClick={() => props.changeBgColor('#301541')} style={{ backgroundColor: '#301541' }}></button>
+            <button className="color-btn me-2" onClick={() => props.changeBgColor('#eee9d2')} style={{ backgroundColor: '#eee9d2' }}></button>
         </div>
         {/* <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,93 +1,91 @@
-import React, {useState} from 'react'
-import nlp from 'compromise'
-import PropTypes from 'prop-types';
-
-
+import React, { useState } from 'react';
+import nlp from 'compromise';
 
 export default function TextForm(props) {
-    const [text, setText] = useState('')
-    const [ tags, setTags] = useState([])
-    const [previewText, setPreviewText] = useState(''); // State for preview text
+  const [text, setText] = useState('');
+  const [tags, setTags] = useState([]);
+  const [previewText, setPreviewText] = useState(''); // State for preview text
 
-    const clearText= ()=>{
-        console.log("Clear text button clicked!")
-        let newText = '';
-        setText(newText)
-        setPreviewText('Clear Text');
+  const clearText = () => {
+    console.log("Clear text button clicked!");
+    let newText = '';
+    setText(newText);
+    setPreviewText('Clear Text');
+  };
 
-    }
+  const handleUpCase = () => {
+    console.log("Upper case button clicked!");
+    let newText = text.toLocaleUpperCase();
+    setText(newText);
+    setPreviewText('Upper Case');
+  };
 
-    const handleUpCase= ()=>{
-        console.log("uppper case button clicked!")
-        let newText =  text.toLocaleUpperCase();
-        setText(newText)
-        setPreviewText('Upper Case');
+  const handleLoCase = () => {
+    console.log("Lower case button clicked!");
+    let newText = text.toLocaleLowerCase();
+    setText(newText);
+    setPreviewText('Lower Case');
+  };
 
-    }
-    // handleLoCase
-    const handleLoCase= ()=>{
-        console.log("uppper case button clicked!")
-        let newText =  text.toLocaleLowerCase();
-        setText(newText)
-        setPreviewText('Lower Case');
+  const handleOnChane = (event) => {
+    console.log("On change!");
+    setText(event.target.value);
+    setPreviewText('');
+  };
 
-    }
+  // Analyze POS Tags
+  const handlePosTagging = () => {
+    const doc = nlp(text); // Process text with compromise
+    const words = doc.terms().out('array'); // Extract words
+    const results = words.map((word) => {
+      const term = nlp(word);
+      // Identify POS tags
+      if (term.nouns().length) return { word, pos: 'Noun' };
+      return null;
+    }).filter((result) => result !== null);
+    setTags(results); // Update POS tags in state
+  };
 
-    const handleOnChane = (event)=>{
-        console.log("On change!")
-        setText(event.target.value)
-        setPreviewText(''); 
-    }
-
-      // Analyze POS Tags
-    const handlePosTagging = () => {
-        const doc = nlp(text); // Process text with compromise
-        const words = doc.terms().out('array'); // Extract words
-        const results = words.map((word) => {
-            const term = nlp(word);
-            // Identify POS tags
-            if (term.nouns().length) return { word, pos: 'Noun' };
-            // if (term.verbs().length) return { word, pos: 'Verb' };
-            // if (term.adjectives().length) return { word, pos: 'Adjective' };
-            // return { word, pos: 'Other' };
-            return null;
-        }).filter((result) => result !== null);
-        setTags(results); // Update POS tags in state
-    };
-   
-    return (
-        <>
-    <div className='container mt-2'>
-        {/* <div className="mb-3">
-            <label for="exampleFormControlInput1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-            </div> */}
+  return (
+    <>
+      <div className="container mt-2">
         <div className="mb-3">
-         {/* <label  className="form-label"> Text Area</label> */}
-         <textarea className="form-control mb-4" value={text} onChange={handleOnChane} style={{ backgroundColor: props.mode ==='light' ? 'white' : 'grey', color: props.mode ==='light' ? 'black' : 'white' }} id="myBox" rows="8" ></textarea>
-         <button className="btn btn-primary mx-3" onClick={handleUpCase}> Convert to UpperCase</button>
-         <button className="btn btn-primary mx-3" onClick={handleLoCase}> Convert to LowerCase</button>
-         <button className="btn btn-success mx-3" onClick={handlePosTagging}> Analyze POS Tags </button>
-         <button className="btn btn-warning mx-3" onClick={clearText}> Clear Text </button>
+          <textarea className="form-control mb-4" value={text} onChange={handleOnChane}
+            style={{
+              backgroundColor: props.bgColor || 'white', // Use bgColor passed from App.js
+              color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'white' : 'black'
+            }}
+            id="myBox"rows="8" ></textarea>
 
+          <button className="btn btn-primary mx-3" onClick={handleUpCase}>Convert to UpperCase</button>
+          <button className="btn btn-primary mx-3" onClick={handleLoCase}>Convert to LowerCase</button>
+          <button className="btn btn-success mx-3" onClick={handlePosTagging}>Analyze POS Tags</button>
+          <button className="btn btn-warning mx-3" onClick={clearText}>Clear Text</button>
         </div>
-    </div>
-    <div className="container my-2" >
-        <h1 style={{ color: props.mode ==='light' ? 'black' : 'white' }}> your text summary</h1>
-        <p style={{ color: props.mode ==='light' ? 'black' : 'yellow' }}> {text === '' ? text.split(" ").length - 1 : text.split(" ").length} words and {text.length} characters</p>
-        <h2 style={{ color: props.mode ==='light' ? 'black' : 'white' }}> Preview {previewText} </h2>
-        <p style={{ color: props.mode ==='light' ? 'black' : 'yellow' }} >{text}</p>
-    </div>
-    <div className="container my-2" >
-        <h2 style={{ color: props.mode ==='light' ? 'black' : 'white' }}>Nouns Tagging Results</h2>
-        <ul style={{ color: props.mode ==='light' ? 'black' : 'yellow' }}>
-            {tags.map((tag, index) => (
-                <li key={index}>
-                    <strong>{tag.word}</strong>: {tag.pos}
-                </li>
-            ))}
+      </div>
+
+      <div className="container my-2">
+        <h1 style={{color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'white' : 'black' }}>
+            Your text summary</h1>
+        <p style={{color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'yellow' :  props.bgColor ==='white' ? 'grey' : 'white' }}>
+            {text === '' ? text.split(" ").length - 1 : text.split(" ").length} words and {text.length} characters</p>
+        <h2 style={{color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'white' : 'black' }}>
+            Preview {previewText}</h2>
+        <p style={{color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'yellow' : props.bgColor ==='white' ? 'grey' : 'white' }}>
+            {text}</p>
+      </div>
+
+      <div className="container my-2">
+        <h2 style={{color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'white' : 'black' }}>
+            Nouns Tagging Results</h2>
+        <ul style={{color: (props.bgColor === '#022348' || props.bgColor === '#301541') ? 'yellow' : props.bgColor ==='white' ? 'grey' : 'white' }}>
+          {tags.map((tag, index) => (
+            <li key={index}>
+              <strong>{tag.word}</strong>: {tag.pos}
+            </li>
+          ))}
         </ul>
-    </div>
+      </div>
     </>
-  )
+  );
 }
